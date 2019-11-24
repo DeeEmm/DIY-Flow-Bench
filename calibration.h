eec150a02fe0f6e1ddfba0237473c6a4934035f9
DeeEmm <deeemm@deeemm.com>
2019-11-25 10:23:50 +1030

    Update calibration.h
    Added function for bench test

--- calibration.h
+++ calibration.h
@@ -5,28 +5,85 @@
 
 
 /****************************************
- * WRITE CALIBRATION DATA TO NVM
+ * WRITE FLOW CALIBRATION DATA TO NVM
  ***/
-void setCalibration ()
+void setFlowCalibrationValue (int currentFlowValue = 0)
 {
-    //we need to save the calibration data to non-volatile memory on the arduino
+    // we need to save the calibration data to non-volatile memory on the arduino
+
+    // we can return boolean to indicate that write has been successful (error checking)
 }
 
 
 /****************************************
- * GET CALIBRATION DATA FROM NVM
+ * GET FLOW CALIBRATION DATA FROM NVM
  ***/
-void getCalibration ()
+int getFlowCalibration ()
 {
+    int flowCalibrationValue = 0;
     //Retrieve calibration data from NVM
+    return flowCalibrationValue;
+}
+
+
+/****************************************
+ * WRITE LEAK-TEST CALIBRATION DATA TO NVM
+ ***/
+void setLeakCalibration (int currentVacValue = 0)
+{
+    //we need to save the calibration data to non-volatile memory on the arduino
+
+    // we can return boolean to indicate that write has been successful (error checking)
+}
+
+
+/****************************************
+ * CHECK LEAK-TEST CALIBRATION DATA FROM NVM
+ ***/
+bool checkLeakCalibration (int currentVacValue = 0, int toleranceCFM = 0)
+{    
+    int leakCalibrationValue = 0; 
+
+    // get value from NVM
+    // (need to add this)
+    //leakCalibrationValue = ??????;
+
+    // lower the test value to define test tolerance
+    // NOTE, this will essentially determine bench accuracy
+    leakCalibrationValue -= toleranceCFM;
+
+    //compare calibration data from NVM
+    if (leakCalibrationValue >= currentVacValue )
+    {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 
 /****************************************
  * GET FLOW MAP DATA FROM EXTERNAL LOOKUP TABLE
  ***/
-void loadMAFLookupTable ()
+void loadMAFLookupTable (int mafType = 1)
 {
     //Retrieve calibration data from external data file
     //populate array [voltage, cfm]
+
+    //select MAF Type
+    switch(mafType) {
+
+        case 1 : //replace with constant to make code easier to read]
+            //read data from file (0.1v increments)
+            //write to array [voltage, cfm]
+            //return array
+        break;
+
+        case 2 : //additional MAF data as required
+            
+        break;
+
+    }
+
 }
