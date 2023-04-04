18de31b1a4346bb35d3098d0589da4f39b42be61
DeeEmm <deeemm@deeemm.com>
2023-04-04 12:53:45 +0930

    Build 23040401

--- ESP32/DIY-Flow-Bench/configuration.h
+++ ESP32/DIY-Flow-Bench/configuration.h
@@ -26,7 +26,7 @@
 
 #define MAJOR_VERSION "V2"
 #define MINOR_VERSION "0"
-#define BUILD_NUMBER "23040302"
+#define BUILD_NUMBER "23040401"
 #define RELEASE "V.2.0-RC.6"
 #define DEV_BRANCH "https://github.com/DeeEmm/DIY-Flow-Bench/tree/WIP"
 
